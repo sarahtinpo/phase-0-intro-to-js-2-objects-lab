a6464f1707eab84eed7b7a40629c2aaa671b5a94
Sarah Tan <sadie_red@hotmail.com>
2022-07-17 21:20:41 +1000

    Completed Assignmnet

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+  name: "Sarah",
+  streetAddress: "1 McGhee Avenue",
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+  const newEmployee = { ...employee };
+  newEmployee[key] = value;
+  return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+  obj[key] = value;
+  return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+	const newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+	delete obj[key];
+	return obj;
+}
